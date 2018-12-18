90911a74f7d0696f095c4851ce2725806af18aa2
HanSungHo <gkstjdgh6996@naver.com>
2018-12-18 22:51:40 +0900

    magic

--- 주차장 평균평점 별 거리.sql
+++ 주차장 평균평점 별 거리.sql
@@ -7,5 +7,5 @@ select 주차장.주소, 주차장.종류, 주차장.총_자리_수, 주차장.현재_자리_수,
 FROM 주차장 join (select 주차장.주차장번호,AVG(convert(float,리뷰.평점)) as 주차장평점
 from 주차장 join 리뷰 on 주차장.주차장번호=리뷰.주차장번호
 group by 주차장.주차장번호) as 평균평점 on 주차장.주차장번호=평균평점.주차장번호
-
+where 주차장평점>3
 ORDER BY distance;
